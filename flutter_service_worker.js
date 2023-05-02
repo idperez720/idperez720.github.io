39ab66623550ea1b8a91bd76727633b1336723ba
Ivan Pérez <ivandavidperez4@gmail.com>
2023-05-02 06:28:10 -0500

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,9 +47,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bc732bb6050eb46e1e762e887d322948",
-"/": "bc732bb6050eb46e1e762e887d322948",
-"main.dart.js": "238a1aa1ef8ad648932c0aa0ac31e0d1",
+"index.html": "725f787bad489fc8e0c6f762d28369e6",
+"/": "725f787bad489fc8e0c6f762d28369e6",
+"main.dart.js": "402a0f4e87f44c3d2cf1743d8a7e63bc",
 "manifest.json": "2673c7d3c051d7e9314a4f8ff55c09c4",
 "version.json": "10753d08b182b0c79edab833722c37d9"
 };
